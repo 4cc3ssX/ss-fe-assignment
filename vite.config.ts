a9c04bbdb7a03f8cd6d08db61dca443c8337e051
Ryam <heinmyatthu.workspace@gmail.com>
2026-04-09 13:29:19 +0700

    Revert "fix: update vite base public path"
    This reverts commit 4f337dd06f8a14a344a105aa85d901fc2721c2ec.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-  base: command === "build" ? "ss-fe-assignment" : "/",
-}));
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
